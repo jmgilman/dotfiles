d365da69160aa930434c310e7e58bf6257f9c2ac
Joshua Gilman <joshuagilman@gmail.com>
2022-02-23 11:32:38 -0800

    refactor: uses home-manager with zsh

--- dot_nixpkgs/darwin-configuration.nix
+++ dot_nixpkgs/darwin-configuration.nix
@@ -18,10 +18,12 @@ let syspkgs = [
   pkgs.jq
   pkgs.nano
   pkgs.nodejs
+  pkgs.oh-my-zsh
   pkgs.packer
   pkgs.poetry
   pkgs.postgresql
   pkgs.pre-commit
+  pkgs.pure-prompt
   pkgs.python
   pkgs.redis
   pkgs.rustup
@@ -30,6 +32,7 @@ let syspkgs = [
   pkgs.vault
   pkgs.wget
   pkgs.vim
+  pkgs.zsh
 ];
 in {
   # Default system configurations
@@ -59,10 +62,11 @@ in {
   programs.gnupg.agent.enable = true;
   programs.gnupg.agent.enableSSHSupport = true;
   programs.nix-index.enable = true;
+  programs.zsh.enable = true;
 
   # Environment configuration
-  environment.loginShell = "${pkgs.bash}/bin/bash";
-  environment.variables.SHELL = "${pkgs.bash}/bin/bash";
+  environment.loginShell = "${pkgs.zsh}/bin/zsh -l";
+  environment.variables.SHELL = "${pkgs.zsh}/bin/zsh";
   environment.variables.LANG = "en_US.UTF-8";
 
   # List packages installed in system profile. To search by name, run:
@@ -78,7 +82,7 @@ in {
   # nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  # programs.zsh.enable = true;  # default shell on catalina
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
@@ -89,4 +93,82 @@ in {
     extra-platforms = aarch64-darwin x86_64-darwin
     experimental-features = nix-command flakes
   '';
+
+  # Home Manager
+  imports = [ <home-manager/nix-darwin> ];
+  users.users.josh = {
+    name = "josh";
+    home = "/Users/josh";
+  };
+
+  home-manager.users.josh = { pkgs, ... }: {
+    home.packages = [  ];
+    programs.zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "ansible"
+          "aws"
+          "copydir"
+          "copyfile"
+          "copybuffer"
+          "dirhistory"
+          "git"
+          "macos"
+          "sudo"
+          "terraform"
+          "web-search"
+          "vscode"
+        ];
+        theme = "";
+      };
+
+      plugins = [
+        {
+          name = "you-should-use";
+          src = pkgs.fetchFromGitHub {
+            owner = "MichaelAquilina";
+            repo = "zsh-you-should-use";
+            rev = "1.7.3";
+            sha256 = "/uVFyplnlg9mETMi7myIndO6IG7Wr9M7xDFfY1pG5Lc=";
+          };
+        }
+      ];
+
+      envExtra = ''
+        # Load exports
+        source .exports
+      '';
+
+      initExtra = ''
+        # Setup pyenv
+        eval "$(pyenv init --path)"
+        eval "$(pyenv init -)"
+
+        # Setup pure
+        fpath+=${pkgs.pure-prompt}/share/zsh/site-functions
+        autoload -U promptinit; promptinit
+        prompt pure
+        zstyle :prompt:pure:path color green
+
+        # Setup bitwarden
+        eval "$(bw completion --shell zsh); compdef _bw bw;"
+
+        # Setup Hashicorp autocompletion
+        complete -o nospace -C ${pkgs.consul}/bin/consul consul
+        complete -o nospace -C ${pkgs.packer}/bin/packer packer
+      '';
+
+      initExtraBeforeCompInit = ''
+        # Add completions
+        fpath+=${pkgs.chezmoi}/share/zsh/site-functions
+        fpath+=${pkgs.google-cloud-sdk}/share/zsh/site-functions
+        fpath+=${pkgs.poetry}/share/zsh/vendor-completions
+        fpath+=/opt/homebrew/share/zsh/site-functions
+      '';
+    };
+  };
 }
